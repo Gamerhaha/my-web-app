cff8d4c47033fa433c18270e6dcd4623ac0efe52
SUGINAKASH <nebalsugin@gmail.com>
2025-11-10 11:16:25 +0530

    w

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.get('/', (req, res) => {
             </svg>
           </div>
           <h1>CI/CD Pipeline</h1>
-          <p class="subtitle">yayy! The Deployment is Successful</p>
+          <p class="subtitle">yayy! the Deployment is Successful</p>
         </div>
 
         <div class="info-card">
